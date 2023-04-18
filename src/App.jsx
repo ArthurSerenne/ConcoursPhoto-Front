ad1300cf8f829d8046c37f162af741273a898fde
Aleksandar16 <a.milenkovic321@gmail.com>
2023-04-18 11:45:04 +0200

    fix home

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Contest from "./pages/Contest";
 import ViewContest from "./pages/ViewContest";
+import ListContest from "./pages/ListContest";
+import Home from "./pages/Home";
 
 const App = () => {
     return (
         <div className="App">
             <Navbar />
             <Routes>
-                <Route path="/contest" element={<Contest />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/contest" element={<ListContest />} />
                 <Route path="/contest/:id" element={<ViewContest />} />
             </Routes>
             <Footer />
