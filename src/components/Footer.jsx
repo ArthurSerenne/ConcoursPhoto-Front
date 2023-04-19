ad6b0a02f9127d5460d6159c4af3d98927b7db28
Arthur Serenne <arthur.serenne@gmail.com>
2023-04-19 15:22:40 +0200

    ajout des pubs dans le footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,18 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
         <footer>
+            <div className=" max-w-screen-2xl mx-auto mt-10 mb-12 flex flex-wrap justify-between items-center">
+                <div className=" w-[1100px] h-[100%] px-[360px] py-36 box-border  bg-[rgba(217,217,217,1)]">
+                    <p className=" flex flex-col justify-center  border-[#000000ff] text-xs leading-3  font-inter  font-[400] text-center">
+                    Pub home footer gauche
+                    </p>
+                </div>
+                <div className=" w-[419px] h-[100%] px-[147px] py-36 box-border  bg-[rgba(217,217,217,1)]">
+                    <p className=" flex flex-col justify-center  border-[#000000ff] text-xs leading-3  font-inter  font-[400] text-center">
+                    Pub home footer droit
+                    </p>
+                </div>
+            </div>
             <ul className='flex flex-row justify-center items-center gap-30 mb-10'>
                 <li className="font-bold not-italic text-sm">
                     <Link to="/">ConcoursPhotos.com &copy; Tous droits réservés</Link>
