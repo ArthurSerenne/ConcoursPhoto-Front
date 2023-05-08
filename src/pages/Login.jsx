5f3936634f103194fa1836e98e1bb4c9b7ca7e4b
Aleksandar16 <a.milenkovic321@gmail.com>
2023-05-08 22:24:40 +0200

    test

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -26,16 +26,9 @@ const Login = ({ closeModal, openRegisterModal }) => {
           </a>
           , c'est gratuit.
         </p>
-    <div className="text-center bg-red">
-      <div className='mb-5'>
-      <h1 className='not-italic font-bold text-text-2xl leading-[29px] flex items-center text-black mb-5 ml-12'>Connexion</h1>
-        <p className='not-italic font-bold text-sm leading-[22px] flex items-center text-black ml-12'>Veuillez vous identifier pour pouvoir voter et participer.</p>
-        <p className='not-italic font-normal text-sm leading-[22px] flex items-center text-black ml-12'>Si vous n'avez pas de compte, inscrivez-vous, c'est gratuit.</p>
       </div>
       <LoginForm closeModal={closeModal} />
-      <p className="mt-5 text-sm font-normal not-italic leading-[17px] text-black">
-        Vous avez oublié votre mot de passe ? Cliquez ici
-      </p>
+      <p className='not-italic font-normal text-sm mt-5 leading-[17px] text-black'>Vous avez oublié votre mot de passe ? Cliquez ici</p>
     </div>
   );
 };
